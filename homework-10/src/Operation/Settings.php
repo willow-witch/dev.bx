1809d3b931cb71d7c544710f6b5c9c90cb2d8f12
willow-witch <sealana048@gmail.com>
2021-12-10 21:40:36 +0200

    homework-10: добавление и тестирование AfterActions в Settings

---
+++ homework-10/src/Operation/Settings.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Operation;
+
+class Settings
+{
+	protected $isBeforeActionsEnabled = true;
+	protected $isAfterActionsEnabled = true;
+
+
+	public function disableBeforeSaveActions(): self
+	{
+		$this->isBeforeActionsEnabled = false;
+
+		return $this;
+	}
+
+	public function disableAfterSaveActions(): self
+	{
+		$this->isAfterActionsEnabled = false;
+
+		return $this;
+	}
+
+	public function isBeforeActionsEnabled(): bool
+	{
+		return $this->isBeforeActionsEnabled;
+	}
+
+	public function isAfterActionsEnabled(): bool
+	{
+		return $this->isAfterActionsEnabled;
+	}
+
+}
